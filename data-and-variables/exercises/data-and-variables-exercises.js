4223e1af3cb8b2ff86e5b843a6bb36dfeddf319f
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-05-21 15:48:01 -0500

    Exercise Completed

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,33 @@
 // Declare and assign the variables below
+let shuttlename = "determination";
+let shuttlespeedmph = 17500;
+let kilometerstomars = 225000000;
+let kilometerstomoon = 384400;
+let milesperkilometer = 0.621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttlename);
+console.log(typeof shuttlespeedmph);
+console.log(typeof kilometerstomars);
+console.log(typeof kilometerstomoon);
+console.log(typeof milesperkilometer);
 
 // Calculate a space mission below
+let milestomars = kilometerstomars * milesperkilometer;
+let hourstomars = milestomars / shuttlespeedmph;
+let daystomars = hourstomars / 24;
 
 // Print the results of the space mission calculations below
+console.log(shuttlename, "will take", daystomars, "days to reach Mars.");
 
 // Calculate a trip to the moon below
+let milestomoon = kilometerstomoon * milesperkilometer;
+let hourstomoon = milestomoon / shuttlespeedmph;
+let daystomoon = hourstomoon / 24;
 
 // Print the results of the trip to the moon below
+console.log(
+  shuttlename + " will take " + daystomoon + "days to reach the Moon."
+);
+
+//Exercise completed.
--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,16 @@
-   // This demo shows off comments!
+// This demo shows off comments!
 
-   // console.log("This does not print.");
+// console.log("This does not print.");
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+/* This is 
+   an example 
+   of a multi-line 
+   comment since you
+   never know when 
+   you may need it. */
 
-   console.log("Comments make your code more readable by others.");
+// This is a single line comment. It's the little things that can be the most helpful.//
+
+console.log("Comments make your code more readable by others.");
