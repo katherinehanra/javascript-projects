2b9c86b7ef0fee5ae8d28c93ed09f8c70d25c35f
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-07-21 13:34:41 -0500

    Chapter Example Modified

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,21 +1,25 @@
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
+  constructor(name, age, mass, color) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+    this.color = color;
+  }
 }
 
-let fox = new Astronaut('Fox', 7, 12);
+let fox = new Astronaut("Fox", 7, 12);
 
 console.log(fox);
 console.log(fox.age, fox.color);
 
 fox.age = 9;
-fox.color = 'red';
+fox.color = "red";
 
 console.log(fox);
 console.log(fox.age, fox.color);
 
 //Try modifying or adding properties below.
+let turtle = new Astronaut("Tami the Turtle", 98, 32, "dark green");
+
+console.log(turtle);
